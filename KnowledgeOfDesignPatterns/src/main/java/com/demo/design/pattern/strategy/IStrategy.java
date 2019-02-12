420e3dfc5d242c82edeffd432cfe7493e0b74b79
Je <364049613@qq.com>
2019-02-12 17:58:23 +0800

    feat:设计模式<->策略模式

---
+++ KnowledgeOfDesignPatterns/src/main/java/com/demo/design/pattern/strategy/IStrategy.java
@@ -0,0 +1,13 @@
+package com.demo.design.pattern.strategy;
+
+/**
+ * @author 揭光智
+ * @date 2019/02/12
+ */
+public interface IStrategy {
+
+    /**
+     * 每个锦囊妙计都是一个可执行的算法
+     */
+    void operate();
+}
