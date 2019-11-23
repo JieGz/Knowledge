4b46ad1e2bb94c2f98699e07a5c0833407be003a
Je <364049613@qq.com>
2019-11-23 10:12:09 +0800

    feat:整理一下代码

--- KnowledgeOfJson/src/main/java/com/demo/json/Main.java
+++ KnowledgeOfJson/src/main/java/com/demo/json/Main.java
@@ -1,98 +1,12 @@
 package com.demo.json;
 
-import com.demo.json.entity.Entity;
-import com.demo.json.entity.LeiTai;
-import com.google.gson.Gson;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-
-import java.net.URI;
-import java.nio.ByteBuffer;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * @author 揭光智
  * @date 2019/05/07
  */
-public class Main extends WebSocketClient {
+public class Main {
     public static void main(String[] args) throws Exception {
 
-        //注意注意,并不是每次都能拿到数据的，需要多尝试
-        Main client = new Main(new URI("wss://publicld.gwgo.qq.com?account_value=0&account_type=0&appid=0&token=0"));
-        client.connect();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = new Date(1559563200 * 1000L);
-        System.out.println(System.currentTimeMillis());
-        String time = format.format(date);
-        System.out.println(time);
     }
 
-
-    public Main(URI serverUri) {
-        super(serverUri);
-    }
-
-    @Override
-    public void onOpen(ServerHandshake handshakedata) {
-
-        Entity entity = new Entity();
-        entity.setRequest_type("1004");
-        //entity.setLongtitude(113324773);
-        //entity.setLatitude(23119675);
-        entity.setRequestid(requestId());
-        entity.setPlatform(0);
-        entity.setCfg_type(1);
-        send(covertData(entity));
-        System.out.println("new connection opened");
-    }
-
-    @Override
-    public void onClose(int code, String reason, boolean remote) {
-        System.out.println("closed with exit code " + code + " additional info: " + reason);
-    }
-
-    @Override
-    public void onMessage(String message) {
-        System.out.println("received message: " + message);
-    }
-
-    @Override
-    public void onMessage(ByteBuffer message) {
-        System.out.println("received ByteBuffer");
-
-        System.out.println(parseData(message.array()));
-
-        LeiTai leiTai = new Gson().fromJson(parseData(message.array()), LeiTai.class);
-//        System.out.println(leiTai);
-    }
-
-    @Override
-    public void onError(Exception ex) {
-        System.err.println("an error occurred:" + ex);
-    }
-
-    private long requestId() {
-        return System.currentTimeMillis() % 1234567;
-    }
-
-
-    private byte[] covertData(Entity entity) {
-        String json = new Gson().toJson(entity);
-        System.out.println(json);
-        byte[] jsonBytes = json.getBytes();
-        byte[] data = new byte[jsonBytes.length + 4];
-        data[0] = 0;
-        data[1] = 0;
-        data[2] = 0;
-        data[3] = (byte) jsonBytes.length;
-        System.arraycopy(jsonBytes, 0, data, 4, jsonBytes.length);
-        return data;
-    }
-
-    private String parseData(byte[] array) {
-        byte[] data = new byte[array.length - 4];
-        System.arraycopy(array, 4, data, 0, array.length - 4);
-        return new String(data);
-    }
 }
--- KnowledgeOfParse/src/main/java/com/demo/Main.java
+++ KnowledgeOfParse/src/main/java/com/demo/Main.java
@@ -1,8 +1,104 @@
 package com.demo;
 
-/**
- * @author 揭光智
- * @date 2019/10/11
- */
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
+
+    private static final String DEVICE_DELAY = "device.delay";
+    private static final String DEVICE_CLICK = "device.click(";
+    private static final String DEVICE_STATE_DOWN = ", sigmaConst.STATE_DOWN";
+    private static final String DEVICE_STATE_UP = ", sigmaConst.STATE_UP";
+    private static final String DEVICE_SWIPE = "device.swipe([";
+    private static final String STEP_CLICK = "click(";
+
+    private static int tmpTime = 0;
+    private static final int USEFUL_DELAY_TIME = 200;
+
+    public static void main(String[] args) {
+        convert("D:\\work\\project\\idea\\Knowledge\\KnowledgeOfParse\\src\\main\\java\\com\\demo\\a.txt", "d://b.txt");
+    }
+
+
+    private static String jsConvertJava(String content) {
+        if (content.contains(DEVICE_DELAY)) {
+            String replace = content.replace(DEVICE_DELAY + "(", "");
+            replace = replace.replace(");", "");
+            int time = Integer.valueOf(replace.trim());
+            content = "";
+            if (time > USEFUL_DELAY_TIME) {
+                tmpTime = time;
+            }
+        } else if (content.contains(DEVICE_CLICK) && content.contains(DEVICE_STATE_DOWN)) {
+            content = content.replace(DEVICE_CLICK, STEP_CLICK + tmpTime + "L, ");
+            content = content.replace(DEVICE_STATE_DOWN, "");
+            tmpTime = 0;
+        } else if (content.contains(DEVICE_CLICK) && content.contains(DEVICE_STATE_UP)) {
+            content = "";
+        }/* else if (content.contains(DEVICE_SWIPE)) {
+            content = content.replace(DEVICE_SWIPE, "");
+            content = content.replace("]);", "");
+            System.out.println(content);
+            String[] split = content.split("],");
+            int length = split.length;
+            String first = split[0];
+            String last = split[length - 1];
+
+            first = first.replace("[", "");
+            first = first.replace("]", "");
+            String[] firstArr = first.split(",");
+
+            last = last.replace("[", "");
+            last = last.replace("]", "");
+            String[] lastArr = last.split(",");
+
+            content = "slide(" + tmpTime + "L, " + firstArr[0] + ", " + firstArr[1] + ", " + lastArr[0] + ", " + lastArr[1] + ");";
+        }*/
+        if (!"".equals(content)) {
+            return content + "\r\n";
+        }
+        return "";
+    }
+
+
+    private static void convert(String originFile, String productFile) {
+        try (FileInputStream inputStream = new FileInputStream(originFile); BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                String content = jsConvertJava(str);
+                if (!"".equals(content)) {
+                    saveAsFileWriter(productFile, content);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void saveAsFileWriter(String filePath, String content) {
+        File file = new File(filePath);
+        if (!file.exists()) {
+            try {
+                boolean b = file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        FileWriter fwriter = null;
+        try {
+            fwriter = new FileWriter(filePath, true);
+            fwriter.write(content);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                fwriter.flush();
+                fwriter.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
