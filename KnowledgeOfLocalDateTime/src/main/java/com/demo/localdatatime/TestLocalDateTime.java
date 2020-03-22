6d65741b2ef578895003b8329a223ef011923547
Luke <364049613@qq.com>
2020-03-22 10:25:20 +0800

    feat:增加格式化时间

--- KnowledgeOfLocalDateTime/src/main/java/com/demo/localdatatime/TestLocalDateTime.java
+++ KnowledgeOfLocalDateTime/src/main/java/com/demo/localdatatime/TestLocalDateTime.java
@@ -1,6 +1,7 @@
 package com.demo.localdatatime;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author 揭光智
@@ -8,10 +9,15 @@ import java.time.LocalDateTime;
  */
 public class TestLocalDateTime {
     public static void main(String[] args) {
-        LocalDateTime now = LocalDateTime.of(2019, 03, 25, 00, 00, 00);
+        /*LocalDateTime now = LocalDateTime.of(2019, 03, 25, 00, 00, 00);
         System.out.println(now);
         System.out.println(now.minusDays(2).plusHours(5));
-        System.out.println(now.minusDays(9).plusHours(6));
+        System.out.println(now.minusDays(9).plusHours(6));*/
 
+
+        //格式化时间
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String startTime = LocalDateTime.now().format(formatter);
+        System.out.println(startTime);
     }
 }
