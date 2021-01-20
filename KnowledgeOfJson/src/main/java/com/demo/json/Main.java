c090949b4b0497af3f0265a85fdb9014f07d8f3b
jieguangzhi <jieguangzhi@yy.com>
2021-01-20 11:08:14 +0800

    feat: Objects的equals方法测试

--- KnowledgeOfJson/src/main/java/com/demo/json/Main.java
+++ KnowledgeOfJson/src/main/java/com/demo/json/Main.java
@@ -1,12 +1,18 @@
 package com.demo.json;
 
+import java.util.Objects;
+
 /**
  * @author 揭光智
  * @date 2019/05/07
  */
 public class Main {
     public static void main(String[] args) throws Exception {
-
+        if (Objects.equals(null, null)) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 
 }
