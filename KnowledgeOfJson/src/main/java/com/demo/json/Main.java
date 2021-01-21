1641bbcca1a1ab9692234572e5565ba95bef9564
jieguangzhi <jieguangzhi@yy.com>
2021-01-21 17:38:26 +0800

    feat: BigDecimal的一些方法使用

--- KnowledgeOfJson/src/main/java/com/demo/json/Main.java
+++ KnowledgeOfJson/src/main/java/com/demo/json/Main.java
@@ -1,6 +1,10 @@
 package com.demo.json;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author 揭光智
@@ -13,6 +17,14 @@ public class Main {
         } else {
             System.out.println("false");
         }
+
+        System.out.println(new BigDecimal("5").subtract(new BigDecimal("1")).toString());
+
+        System.out.println(BigDecimal.valueOf(Double.parseDouble("2.305000")).stripTrailingZeros().toPlainString());
+        System.out.println(BigDecimal.valueOf(Double.parseDouble("3.0000")).stripTrailingZeros().toPlainString());
+
+        System.out.println(new BigDecimal(8).divide(new BigDecimal(2), 4, BigDecimal.ROUND_HALF_UP).subtract(new BigDecimal(1)).toString());
+
     }
 
 }
